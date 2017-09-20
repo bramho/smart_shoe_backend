ceed89384fab9a553748f294a2e956b62175db9f
Grond <gron1300@rc14230.nhl.nl>
2017-09-20 13:24:28 +0200

    Initial push of Xcode Project

---
+++ SmartShoeBackend/SmartShoeBackend/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartShoeBackend
+//
+//  Created by Grond, H. on 20/09/2017.
+//  Copyright © 2017 MTNW07-17. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
