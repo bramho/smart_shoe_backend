3bb05b10d531a8247c3da0deea769832c1224ae5
Grond <gron1300@rc14230.nhl.nl>
2017-09-20 14:58:28 +0200

    Implement CoreBluetooth implementations of BLE functionality for the app (untested)

--- SmartShoeBackend/SmartShoeBackend/ViewController.swift
+++ SmartShoeBackend/SmartShoeBackend/ViewController.swift
@@ -7,18 +7,106 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
+class ViewController:
+    UIViewController,
+    CBCentralManagerDelegate,
+    CBPeripheralDelegate {
+    
+    var manager: CBCentralManager!
+    var peripheral: CBPeripheral!
+    
+    let BEAN_NAME = "Test"
+    let BEAN_SCRATCH_UUID = CBUUID(string: "a495ff21-c5b1-4b44-b512-1370f02d74de");
+    let BEAN_SERVICE_UUID = CBUUID(string: "a495ff21-c5b1-4b44-b512-1370f02d74de")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        manager = CBCentralManager(delegate: self, queue: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == CBManagerState.poweredOn {
+            central.scanForPeripherals(withServices: nil, options: nil)
+        } else {
+            print("Bluetooth not available.")
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager,
+                        didDiscover peripheral: CBPeripheral,
+                        advertisementData: [String : Any],
+                        rssi RSSI: NSNumber) {
+        let device = (advertisementData as NSDictionary)
+        .object(forKey: (CBAdvertisementDataLocalNameKey)) as? NSString
+        
+        if device?.contains(BEAN_NAME) == true {
+            self.manager.stopScan()
+            
+            self.peripheral = peripheral
+            
+            self.peripheral.delegate = self
+            
+            manager.connect(peripheral, options: nil)
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager,
+                        didConnect peripheral: CBPeripheral) {
+        peripheral.discoverServices(nil);
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral,
+                    didDiscoverServices error: Error?) {
+        for service in peripheral.services! {
+            let thisService = service as CBService
+            
+            if service.uuid == BEAN_SERVICE_UUID {
+                peripheral.discoverCharacteristics(nil, for: thisService)
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral,
+                    didDiscoverCharacteristicsFor service: CBService,
+                    error: Error?) {
+        for characteristic in service.characteristics! {
+            let thisCharacteristic = characteristic as CBCharacteristic
+            
+            if thisCharacteristic.uuid == BEAN_SCRATCH_UUID {
+                self.peripheral.setNotifyValue(true, for: thisCharacteristic)
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral,
+                    didUpdateValueFor characteristic: CBCharacteristic,
+                    error: Error?) {
+        //var count:UInt32 = 0;
+        
+        if characteristic.uuid == BEAN_SCRATCH_UUID {
+            print(characteristic.value.debugDescription)
+            //labelCount.text = NSString(format: "%llu" , count) as String
+        }
+        
+    }
+    
+    func centralManager(_ central: CBCentralManager,
+                        didDisconnectPeripheral peripheral: CBPeripheral,
+                        error: Error?) {
+        central.scanForPeripherals(withServices: nil, options: nil)
+    }
+    
+    
+    
+    
 
 
 }
