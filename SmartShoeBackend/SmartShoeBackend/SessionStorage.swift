38f11e06d14ef5f42a2bcaad2179c2d35870eab0
heercogrond <gron1300@student.nhl.nl>
2017-12-19 12:52:38 +0100

    added session storage properly now

---
+++ SmartShoeBackend/SmartShoeBackend/SessionStorage.swift
@@ -0,0 +1,89 @@
+import Foundation
+
+class SessionStorage { 
+
+    var sessions : [Session]?
+    var currentSession : Session?
+
+    init() {
+        super.init()
+
+        sessions = loadSessions()
+    }
+    
+    private func saveSessions() { 
+        for session in sessions { 
+            if (session.dataLeftShoe.count != session.dataRightShoe){
+                if(session != currentSession) {
+                    print(session.sessionDate)
+                    print(currentSession.sessionDate) 
+                    sessions = sessions.filter() { $0 !== session }
+                }
+            }
+        }
+
+        let isSuccesfulSave = NSKeyedArchiver.archiveRootObject(sessions, toFile: Session.ArchiveURL.path)
+        print(isSuccesfulSave)
+    }
+
+    private func loadSessions() -> [Session]? { 
+        return NSKeyedArchiver.unarchiveObject(withFile: Session.ArchiveURL.path) as? [Session]
+    }
+
+    func recordSession(shoeType: Int, shoeData: [Double]) {
+        if(currentSession = nil) { 
+            currentSession = Session.init(date: Date.init())
+            sessions.append(currentSession)
+        }
+
+        currentSession.update(shoeType: shoeType, newData: shoeData)
+    }
+
+    func stopRecordingSession() { 
+
+        saveSessions()
+        currentSession = nil
+    }
+}
+
+class Session: NSObject, NSCoding { 
+
+    var sessionDate: Date!
+    var dataLeftShoe: [[Double]] = []
+    var dataRightShoe: [[Double]] = []
+
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("repressSessions")
+
+    init(date: Date!) {
+        super.init();
+        sessionDate = date
+    }
+    
+    update(shoeType: Int, newData: [Double]) { 
+        if(shoeType == 1) { 
+            dataLeftShoe.append(newData)
+        } else if (shoeType == 2 ) { 
+            dataRightShoe.append(newData)
+        }
+    }
+
+    //NSCoding functions
+
+    encode(with aCoder: NSCoder) {
+        aCoder.encode(sessionDate, forKey: SessionPropertyKeys.sessionDate) 
+        aCoder.encode(dataLeftShoe, forKey: SessionPropertyKeys.dataLeftShoe)
+        aCoder.encode(dataRightShoe, forKey: SessionPropertyKeys.dataRightShoe)
+    }
+
+    init?(coder aDecoder: NSCoder) { 
+
+    }
+
+}
+
+struct SessionPropertyKeys { 
+    static let sessionDate = "sessionDate"
+    static let dataLeftShoe = "dataLeftShoe"
+    static let dataRightShoe ="dataRightShoe"
+}
